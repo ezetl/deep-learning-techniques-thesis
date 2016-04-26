9a383b70d6f9ed32d93736ed3614bcdb37e09b9f
ezetl <ezetl91@gmail.com>
2016-04-26 14:32:46 -0300

    [MNIST] added labels parsing

--- experiments/mnist/preprocess_mnist.cpp
+++ experiments/mnist/preprocess_mnist.cpp
@@ -7,6 +7,8 @@
 
 #include <string> 
 /*
+ * Author: Ezequiel Torti Lopez
+ *
  * This code parses the MNIST dataset files (images and labels).
  * It was done for my low-endian machine, but you can set the LOW_ENDIAN
  * flag off and it will run in high endian mode
@@ -14,7 +16,6 @@
  * How to compile:
  *     g++ -o preprocess_mnist preprocess_mnist.cpp -std=gnu++11 -lopencv_core -lopencv_highgui 
  *
- * Author: Ezequiel Torti Lopez
  */
 
 using namespace std;
@@ -29,6 +30,7 @@ using namespace cv;
 #define TEST_LABELS  (DATA_ROOT"t10k-labels-idx1-ubyte")
 
 typedef char Byte;
+typedef unsigned char uByte;
 typedef struct
 {
     int32_t magic;
@@ -38,80 +40,54 @@ typedef struct
 } MNIST_metadata;
 
 int32_t get_int32_t(ifstream &f, streampos offset);
+vector<uByte> read_block(ifstream &f, unsigned int size, streampos offset);
 MNIST_metadata parse_images_header(ifstream &f);
 MNIST_metadata parse_labels_header(ifstream &f);
-vector<Byte> read_block(ifstream &f, unsigned int size, streampos offset);
-void parse_images(ifstream &f, MNIST_metadata meta, vector<Mat> *mnist);
-void load_images(string path);
-void load_labels(string path);
+void parse_images_data(ifstream &f, MNIST_metadata meta, vector<Mat> &mnist);
+void parse_labels_data(ifstream &f, MNIST_metadata meta, vector<uByte> &labels);
+vector<Mat> load_images(string path);
+vector<uByte> load_labels(string path);
 void process_images();
 void process_labels();
 
 int main()
 {
-    load_images(TRAIN_IMAGES);
-    
+    vector<Mat> train_imgs = load_images(TRAIN_IMAGES);
+    vector<Mat> test_imgs = load_images(TEST_IMAGES);
+    vector<uByte> train_labels = load_labels(TRAIN_LABELS);
+    vector<uByte> test_labels = load_labels(TEST_LABELS);
+    // TODO: add random rotation/translations.
+    // TODO: save Mat to lmdb.
     return 0;
 }
 
-void load_images(string path)
+vector<Mat> load_images(string path)
 {
     ifstream f;
     f.open(path, ios::in | ios::binary);
 
     MNIST_metadata meta = parse_images_header(f);
-    cout << "Magic number: " << meta.magic << endl; 
+    cout << "\nMagic number: " << meta.magic << endl; 
     cout << "Number of Images: " << meta.num_elems << endl; 
     cout << "Rows: " << meta.rows << endl;
     cout << "Columns: " << meta.cols << endl; 
-    vector<Mat> *mnist;
-    parse_images(f, meta, mnist);
+    vector<Mat> mnist(meta.num_elems);
+    parse_images_data(f, meta, mnist);
+    return mnist;
 }
 
-void parse_images(ifstream &f, MNIST_metadata meta, vector<Mat> *mnist)
+vector<uByte> load_labels(string path)
 {
-    unsigned int size_img = meta.cols * meta.rows;
-    // 4 integers in the header of the images file
-    streampos offset = sizeof(int32_t) * 4;
-    for (unsigned int i=0; i<meta.num_elems; i++)
-    {
-        vector<Byte> raw_data = read_block(f, size_img, offset);
-        Mat mchar(raw_data, false);
-        mchar = mchar.reshape(1, meta.rows);
-        //string name = to_string(i);
-        //name += ".png";
-        //imwrite(name, mchar);
-        offset += size_img;
-    }
-}
+    ifstream f;
+    f.open(path, ios::in | ios::binary);
 
-vector<Byte> read_block(ifstream &f, unsigned int size, streampos offset)
-{
-    Byte* bytes; 
-    bytes = (Byte*) malloc(size*sizeof(Byte));
+    MNIST_metadata meta = parse_labels_header(f);
+    cout << "\nMagic number: " << meta.magic << endl; 
+    cout << "Number of Labels: " << meta.num_elems << endl; 
+    vector<uByte> labels_mnist(meta.num_elems);
+    parse_labels_data(f, meta, labels_mnist);
+    return labels_mnist;
 
-    f.seekg(offset);
-    f.read(bytes, size);
-
-    vector<Byte> raw_data(size);
-    for (unsigned int i=0; i<size; i++)
-    {
-        raw_data[i] = bytes[i];
-    }
-
-    free(bytes);
-
-    return raw_data;
-}
-
-MNIST_metadata parse_labels_header(ifstream &f)
-{
-    MNIST_metadata meta;
-    streampos offset = 0;
-    meta.magic = get_int32_t(f, offset);
-    offset += sizeof(int32_t);
-    meta.num_elems = get_int32_t(f, offset);
-    return meta;
 }
 
 MNIST_metadata parse_images_header(ifstream &f)
@@ -126,8 +102,69 @@ MNIST_metadata parse_images_header(ifstream &f)
     offset += sizeof(int32_t);
     meta.cols = get_int32_t(f, offset);
     return meta;
-} 
+}
 
+void parse_images_data(ifstream &f, MNIST_metadata meta, vector<Mat> &mnist)
+{
+    //namedWindow("MNIST");
+    unsigned int size_img = meta.cols * meta.rows;
+    // 4 integers in the header of the images file
+    streampos offset = sizeof(int32_t) * 4;
+    for (unsigned int i=0; i<meta.num_elems; i++)
+    {
+        vector<uByte> raw_data = read_block(f, size_img, offset);
+        Mat mchar(raw_data, false);
+        mchar = mchar.reshape(1, meta.rows);
+        mnist[i] = mchar;
+        //imshow("MNIST", mchar);
+        //waitKey(100);
+        offset += size_img;
+    }
+}
+
+MNIST_metadata parse_labels_header(ifstream &f)
+{
+    MNIST_metadata meta;
+    streampos offset = 0;
+    meta.magic = get_int32_t(f, offset);
+    offset += sizeof(int32_t);
+    meta.num_elems = get_int32_t(f, offset);
+    return meta;
+}
+
+void parse_labels_data(ifstream &f, MNIST_metadata meta, vector<uByte> &labels)
+{
+    // 4 integers in the header of the images file
+    streampos offset = sizeof(int32_t) * 2;
+    for (unsigned int i=0; i<meta.num_elems; i++)
+    {
+        f.seekg(offset);
+        uByte label;
+        f.read((Byte*) &label, sizeof(uByte));
+        labels[i] = label;
+        offset += sizeof(uByte);
+    }
+}
+
+vector<uByte> read_block(ifstream &f, unsigned int size, streampos offset)
+{
+    Byte* bytes; 
+    bytes = (Byte*) malloc(size*sizeof(uByte));
+
+    f.seekg(offset);
+    f.read(bytes, size);
+
+    vector<uByte> raw_data(size);
+    for (unsigned int i=0; i<size; i++)
+    {
+        raw_data[i] = (uByte) bytes[i];
+    }
+
+    free(bytes);
+
+    return raw_data;
+}
+ 
 /*
  * It parses a int (32 bits) from the file f.
  * The MNIST dataset uses big-endian. This function take into account
@@ -155,11 +192,6 @@ int32_t get_int32_t(ifstream &f, streampos offset)
     free(b_int);
 
     return res;
-}
-
-void load_labels(string path)
-{
-
 }
 
 void process_images()
