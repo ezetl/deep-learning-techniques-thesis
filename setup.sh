c2cf073475d1fb9d035baf0133b36b8ad53589a4
ezetl <ezetl91@gmail.com>
2016-05-17 15:08:11 -0300

    added requirements.txt

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 # Install dependencies to build the report (I use Emacs Orgmode to write plain
 # text and then generate a .tex from it)
-sudo apt-get install texlive-base texlive-latex-extra texlive-lang-spanish dvipng
+sudo apt-get install texlive-base texlive-latex-extra texlive-lang-spanish dvipng python-pip
+pip install -r requirements.txt
