fc9d0c6816a7684f50ca5e2b1deed5ca5463d76a
ezetl <ezetl91@gmail.com>
2016-05-17 15:08:31 -0300

    fix in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 # Install dependencies to build the report (I use Emacs Orgmode to write plain
 # text and then generate a .tex from it)
 sudo apt-get install texlive-base texlive-latex-extra texlive-lang-spanish dvipng python-pip
-pip install -r requirements.txt
+sudo pip install -r requirements.txt
