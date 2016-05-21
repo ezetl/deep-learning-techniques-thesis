960a6965ab54ab99168690c2e0d1a0641c652df2
Ezequiel <ezetl91@gmail.com>
2016-05-21 16:24:49 -0300

    new requirements added

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 # Install dependencies to build the report (I use Emacs Orgmode to write plain
 # text and then generate a .tex from it)
 sudo apt-get install texlive-base texlive-latex-extra texlive-lang-spanish dvipng python-pip
+# Pillow dependencies
+sudo apt-get install libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
 sudo pip install -r requirements.txt
