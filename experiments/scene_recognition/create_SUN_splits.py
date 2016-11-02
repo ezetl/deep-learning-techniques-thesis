b6ef0dfa8e0bd6c29c26162d714ae17b4f3fb32f
Ezequiel <ezetl91@gmail.com>
2016-11-02 03:44:12 -0300

    modified create_SUN_splits to create splits with N images per class (as in the paper)

--- experiments/scene_recognition/create_SUN_splits.py
+++ experiments/scene_recognition/create_SUN_splits.py
@@ -2,10 +2,12 @@
 from sys import argv, exit
 from os import makedirs
 from os.path import isdir, join, dirname
+from collections import defaultdict
 
 DATA = "./data/paths"
 SPLITS = [1, 2, 3]
 SPLITS_NAMES = ["Training_", "Testing_"]
+IMGS_PER_CLASS = [5, 20]
 
 if __name__ == "__main__":
     if len(argv) < 2:
@@ -26,12 +28,16 @@ if __name__ == "__main__":
 
     for split in SPLITS:
         for prefix in SPLITS_NAMES:
-            splitfile = prefix + "0" * (split != 10) + str(split) + ".txt"
-            with open(join(part_path, splitfile), 'r') as f:
-                list = ["{} {}".format(elem, classes.index(dirname(elem)))
-                        for elem in f.read().splitlines()]
-                list = "\n".join(list)
-                with open(join(DATA, splitfile), 'w') as out:
-                    out.write(list)
-
+            for im_per_class in IMGS_PER_CLASS:
+                splitfile = prefix + "0" * (split != 10) + str(split)
+                outfile = splitfile + "_" + str(im_per_class) + "per_class.txt"
+                with open(join(part_path, splitfile+".txt"), 'r') as f:
+                    classes_dict = defaultdict(list)
+                    for elem in f.read().splitlines():
+                        classes_dict[classes.index(dirname(elem))].append(elem)
+                    lists_of_imgs = [zip(classes_dict[key][0:im_per_class], [key]*im_per_class) for key in classes_dict]
+                    flattened = ["{} {}".format(item[0], item[1]) for sublist in lists_of_imgs for item in sublist]
+                    flattened = "\n".join(flattened)
+                    with open(join(DATA, outfile), 'w') as out:
+                        out.write(flattened)
     exit(0)
