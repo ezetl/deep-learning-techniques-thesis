2d3936ea1e8e251e2389f2a82a5fab4a93d80c92
Ezequiel <ezetl91@gmail.com>
2016-11-28 00:14:43 -0300

    Modify check lmdb content script to support new lmdb format

--- experiments/visualize_filters/check-lmdb-content.py
+++ experiments/visualize_filters/check-lmdb-content.py
@@ -26,17 +26,23 @@ def print_lmdb_data(lmdb_path):
         datum.ParseFromString(value)
         data = caffe.io.datum_to_array(datum)
         dims = data.shape[0]
-        # If number of dims is 5, it is clearly mnist, since we have 2 images
-        # of one channel each and 3 more channels for the labels
+        # If number of dims is 3, it is clearly mnist, since we have 2 images
+        # of one channel each.
         # If number of dims is 6, it is a data lmdb with 2 images of 3 channels
         # each. This is the new method I'm using to create databases: one LMDB
         # for data, othe LMDB for labels
-        if dims == 5:
+        if dims == 1:
+            cv2.imshow('im1', data[0])
+            cv2.waitKey(0)
+        elif dims == 2:
             cv2.imshow('im1', data[0])
             cv2.imshow('im2', data[1])
             cv2.waitKey(0)
-        elif dims == 1:
-            cv2.imshow('im1', data[0])
+        elif dims == 3:
+            # Assume its a standar lmdb with a 3 channel image
+            im = data.astype(np.uint8)
+            im = np.transpose(im, (1, 2, 0))
+            cv2.imshow('im1', im)
             cv2.waitKey(0)
         elif dims == 6:
             im = data.astype(np.uint8)
@@ -47,13 +53,7 @@ def print_lmdb_data(lmdb_path):
             cv2.imshow('im1', im1)
             cv2.imshow('im2', im2)
             cv2.waitKey(0)
-        elif dims == 3:
-            # Assume its a standar lmdb with a 3 channel image
-            im = data.astype(np.uint8)
-            im = np.transpose(im, (1, 2, 0))
-            cv2.imshow('im1', im)
-            cv2.waitKey(0)
-
+        
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
