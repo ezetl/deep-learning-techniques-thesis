f72acbf23166e797070f8506f2254b5c0d0a5447
Ezequiel <ezetl91@gmail.com>
2017-01-12 23:20:32 -0300

    minor fixes

--- experiments/experiment_kitti.py
+++ experiments/experiment_kitti.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2.7
 from os.path import join, exists
+from os import makedirs
 from optparse import OptionParser, OptionGroup
+import numpy as np
 from utils.nets.cnn_factory import KITTINetFactory 
 from utils.solver.solver import train_net, create_solver_params
 
@@ -41,7 +43,12 @@ if __name__ == "__main__":
     scale = 1.0
     batch_size = 125
     # Train AlexNet on ILSVRC'12 dataset with 20 and 1000 imgs per class 
-    iters = 90000 # TODO: calculate epochs on each dataset
+    iters = 60000 
+    results_path = './results/kitti/'
+    try:
+        makedirs(results_path)
+    except:
+        pass
     imagenet_imgs_per_class = [20, 1000]
     for num in imagenet_imgs_per_class:
         imagenet, loss_blobs_imagenet, acc_blobs_imagenet = KITTINetFactory.standar(
@@ -66,8 +73,8 @@ if __name__ == "__main__":
             snapshot_prefix = 'snapshots/imagenet/imagenet_lmdb{}'.format(num)
             results_imagenet = train_net(create_solver_params(imagenet, test_netspec=imagenet_test, max_iter=iters, test_interv=iters,
                                                                base_lr=0.001, snapshot_prefix=snapshot_prefix),
-                                          loss_blobs=loss_blobs_f, acc_blobs=acc_blobs_f, pretrained_weights=results_ego['snaps'][-1])
-            np.save(join(results_path, 'imagenet_{}.npy'.format(num)))
+                                          loss_blobs=loss_blobs_imagenet, acc_blobs=acc_blobs_imagenet)
+            np.save(join(results_path, 'imagenet_{}.npy'.format(num)), results_imagenet)
 
     ## EGOMOTION NET
     ## Used to train a siamese network from scratch following the method from the 
@@ -86,13 +93,12 @@ if __name__ == "__main__":
     # in finetuning stage 
     iters=60000
     ## Train our first siamese net with Egomotion method
-    results_path = './results/kitti/'
     if exists(join(results_path, 'egomotion.npy')):
         results_ego = np.load(join(results_path, 'egomotion.npy'))
     else:
         results_ego = train_net(create_solver_params(siam_kitti, max_iter=iters, base_lr=0.001, snapshot_prefix='snapshots/kitti/egomotion/kitti_siamese'),
                 loss_blobs=loss_blobs)
-        np.save(join(results_path, 'egomotion.npy'))
+        np.save(join(results_path, 'egomotion.npy'), results_ego)
 
     # CONTRASTIVE NET, m=10
     # Using a small batch size while training with Contrastive Loss leads 
@@ -112,7 +118,7 @@ if __name__ == "__main__":
     else:
         results_contr10 = train_net(create_solver_params(siam_cont10_kitti, max_iter=iters, base_lr=0.001, snapshot_prefix='snapshots/kitti/contrastive/kitti_siamese_m10'),
                 loss_blobs=loss_cont_blobs)
-        np.save(join(results_path, 'contr_10.npy'))
+        np.save(join(results_path, 'contr_10.npy'), results_contr10)
 
     ## CONTRASTIVE NET, m=100
     siam_cont100_kitti, loss_cont_blobs2, acc_cont_blobs2 = KITTINetFactory.siamese_contrastive(
@@ -128,7 +134,7 @@ if __name__ == "__main__":
     else:
         results_contr100 = train_net(create_solver_params(siam_cont100_kitti, max_iter=iters,  base_lr=0.001, snapshot_prefix='snapshots/kitti/contrastive/kitti_siamese_m100'),
                 loss_blobs=loss_cont_blobs2)
-        np.save(join(results_path, 'contr_100.npy'))
+        np.save(join(results_path, 'contr_100.npy'), results_contr100)
     
     sizes_lmdb = ['5', '20'] 
     splits = ['01', '02', '03']
